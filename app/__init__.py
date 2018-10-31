49fdb93a10f682a87fa1753109208a5d776dd348
xingweidong <xingweidong@redoop.com>
2018-10-31 11:47:17 +0800

    更新

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,7 @@ def create_app(config_class=Config):
     def index():
         return render_template('index.html')
 
-    @app.route('/about')
+    @app.route('/about_me')
     def about_me():
         return render_template('about_me.html')
 
