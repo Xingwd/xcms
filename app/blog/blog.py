5f88e135548c76f20cc5b0282527c7e768bf6822
xingweidong <xingweidong@redoop.com>
2018-11-05 20:39:13 +0800

    完成blog首页分页展示功能

--- app/blog/blog.py
+++ app/blog/blog.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, request, current_app, url_for
 )
 from app.models import Post, Tag
 
@@ -7,11 +7,19 @@ from app.models import Post, Tag
 bp = Blueprint('blog', __name__, url_prefix='/blog')
 
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def blog():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.pub_date.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('blog.blog', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('blog.blog', page=posts.prev_num) \
+        if posts.has_prev else None
+
     tags = Tag.query.all()
-    return render_template('blog/blog.html', posts=posts, tags=tags)
+    return render_template('blog/blog.html', posts=posts.items, next_url=next_url,
+                           prev_url=prev_url, tags=tags)
 
 @bp.route('/detail')
 def detail():
--- config.py
+++ config.py
@@ -7,6 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
