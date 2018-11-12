a0d0bb408b7072b4e0e848b20bac0ee362a47707
xingweidong <xingweidong@redoop.com>
2018-11-12 11:24:43 +0800

    更新模型

--- app/models.py
+++ app/models.py
@@ -58,7 +58,7 @@ class User(UserMixin, db.Model):
 
 
 
-tags = db.Table('tags',
+posts_tags = db.Table('posts_tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
     db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True)
 )
@@ -84,7 +84,7 @@ class Post(SearchableMixin, db.Model):
     pub_date = db.Column(db.String(80), index=True)
     pv = db.Column(db.BigInteger, index=True, default=0)
 
-    tags = db.relationship('Tag', secondary=tags, backref=db.backref('posts', lazy='dynamic'))
+    tags = db.relationship('Tag', secondary=posts_tags, backref=db.backref('posts', lazy='dynamic'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.title)
