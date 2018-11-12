7358075762dc4cd922054ad5cb6eaa526b39a0c9
xingweidong <xingweidong@redoop.com>
2018-11-12 10:28:24 +0800

    添加登录表单用户密码错误提示

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,14 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is None:
+            raise ValidationError('Username does not exist.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user is not None:
+            if not user.check_password(password.data):
+                raise ValidationError('Wrong password.')
