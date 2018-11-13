cf85758e6699cbed887341bbe5dc95c205179605
xingweidong <xingweidong@redoop.com>
2018-11-13 11:26:24 +0800

    更新博客搜索功能，使用数据库模糊查询代替elasticsearch，因为机器性能低，无法运行elasticsearch

--- app/blog/blog.py
+++ app/blog/blog.py
@@ -3,6 +3,7 @@ from flask import (
 )
 from app.models import Post, Tag
 from app.forms import SearchForm
+from sqlalchemy import or_
 
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
@@ -68,14 +69,29 @@ def search():
     if not g.search_form.validate():
         return redirect(url_for('blog.blog'))
     page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page,
-                               current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('blog.search', q=g.search_form.q.data, page=page + 1) \
-        if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('blog.search', q=g.search_form.q.data, page=page - 1) \
-        if page > 1 else None
-
     tags = Tag.query.all()
-    return render_template('blog/search.html', posts=posts, next_url=next_url,
-                           prev_url=prev_url, tags=tags)
 
+    if current_app.elasticsearch:
+        posts, total = Post.search(g.search_form.q.data, page,
+                                   current_app.config['POSTS_PER_PAGE'])
+        next_url = url_for('blog.search', q=g.search_form.q.data, page=page + 1) \
+            if total > page * current_app.config['POSTS_PER_PAGE'] else None
+        prev_url = url_for('blog.search', q=g.search_form.q.data, page=page - 1) \
+            if page > 1 else None
+
+        return render_template('blog/search.html', posts=posts, next_url=next_url,
+                               prev_url=prev_url, tags=tags)
+
+    else:
+        posts = Post.query.filter(or_(Post.title.contains(g.search_form.q.data),
+                                             Post.content.contains(g.search_form.q.data))).order_by(
+                                             Post.pub_date.desc()).paginate(
+                                             page, current_app.config['POSTS_PER_PAGE'], False)
+
+        next_url = url_for('blog.search', q=g.search_form.q.data, page=posts.next_num) \
+            if posts.has_next else None
+        prev_url = url_for('blog.search', q=g.search_form.q.data, page=posts.prev_num) \
+            if posts.has_prev else None
+
+        return render_template('blog/search.html', posts=posts.items, next_url=next_url,
+                               prev_url=prev_url, tags=tags)
