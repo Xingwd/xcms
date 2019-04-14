7c86cea1a3caa85c6c0865a0a60d477e5e6d58cb
xingweidong <xingweidong@redoop.com>
2019-04-14 19:18:08 +0800

    更新博客细节逻辑和页面

--- app/tag/tag.py
+++ app/tag/tag.py
@@ -17,14 +17,10 @@ def tag():
 @bp.route('/<name>')
 def tag_filter(name):
     page = request.args.get('page', 1, type=int)
-    posts = Tag.query.filter_by(name=name).first_or_404().posts.order_by(Post.pub_date.desc()).paginate(
+    pagination = Tag.query.filter_by(name=name).first_or_404().posts.order_by(Post.id.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('tag.tag_filter', name=name, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('tag.tag_filter', name=name, page=posts.prev_num) \
-        if posts.has_prev else None
 
     post_count, pv_count = count()
 
-    return render_template('tag/tag_filter.html', posts=posts.items, next_url=next_url,
-                           prev_url=prev_url, post_count=post_count, pv_count=pv_count)
+    return render_template('tag/tag_filter.html', pagination=pagination,
+                           post_count=post_count, pv_count=pv_count)
