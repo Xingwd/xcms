8a4edf17ed6a9bb023158bdd0f6e5c917a1afff3
xingweidong <xingweidong@redoop.com>
2019-06-08 22:46:17 +0800

    开发认证模块

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,31 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask
 from conf.config import ProductionConfig, DevelopmentConfig
+from flask import Flask
+from flask_login import LoginManager
+from pymongo import MongoClient
+from models import User
 
 
-def create_app(Config=ProductionConfig):
+login_manager = LoginManager()
+
+
+def create_app(config=ProductionConfig):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config)
+    # mongo数据库实例
+    db = MongoClient(config.MONGO_HOST, config.MONGO_PORT)[config.XCMS_DB]
+
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(username):  # login_manager 回调函数
+        doc = db[User.tablename].find_one({'username': username})
+        if doc:
+            return doc
+        else:
+            return None
+
+    from server.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
 
--- server/models.py
+++ server/models.py
@@ -1,29 +1,73 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User():
-    """User Model"""
+class User(object):
+    """User Model.
+
+    Parameters
+    ----------
+    username : string
+        User name.
+
+    Attributes
+    ----------
+    tablename : string
+        Name of user table.
+
+    """
 
     tablename = 'user'
 
-    def __init__(self, username, email, password):
+    def __init__(self, username):
         self.username = username
-        self.email = email
-        self.password = password
 
-    def model(self):
-        """Define a model"""
-        model = {
-            'username': self.username,
-            'email': self.email,
-            'password': self.hash_password()
-        }
-        return model
+    @property
+    def is_active(self):
+        return True
 
-    def hash_password(self):
-        """Hash a password"""
-        return generate_password_hash(self.password)
+    @property
+    def is_authenticated(self):
+        return True
 
-    def check_password(self, password):
-        """Check a password"""
-        return check_password_hash(self.hash_password(), password)
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        self.username
+
+    @staticmethod
+    def hash_password(password):
+        """Hash a password.
+
+        Parameters
+        ----------
+        password : string
+            Password of a user.
+
+        Returns
+        -------
+        string
+            Password hash.
+
+        """
+        return generate_password_hash(password)
+
+    @staticmethod
+    def check_password(password_hash, password):
+        """Check a password.
+
+        Parameters
+        ----------
+        password_hash : string
+            A user password_hash in db.
+        password : type
+            A password user typed.
+
+        Returns
+        -------
+        boolen
+            True of False.
+
+        """
+        return check_password_hash(password_hash, password)
