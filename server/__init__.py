c7ebd6348b9339d1552532a9e122754a6cde1c2e
xingweidong <xingweidong@redoop.com>
2019-06-16 11:47:52 +0800

    移除flask-login代码

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,8 @@
 # -*- coding: UTF-8 -*-
 from conf.config import ProductionConfig, DevelopmentConfig
 from flask import Flask
-from flask_login import LoginManager
 from flask_cors import CORS
 from pymongo import MongoClient
-from models import User
-
-
-login_manager = LoginManager()
 
 
 def create_app(config=ProductionConfig):
@@ -17,15 +12,7 @@ def create_app(config=ProductionConfig):
     # mongo数据库实例
     app.db = MongoClient(config.MONGO_HOST, config.MONGO_PORT)[config.XCMS_DB]
 
-    login_manager.init_app(app)
-    @login_manager.user_loader
-    def load_user(username):  # login_manager 回调函数
-        user = app.db[User.tablename].find_one({'username': username})
-        if user:
-            return user
-        else:
-            return None
-
+    # Blueprint
     from auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
