2cdbb776bafb1a8cbed272268b36fdc8dce08094
xingweidong <xingweidong@redoop.com>
2019-06-17 21:12:48 +0800

    更新

--- server/auth.py
+++ server/auth.py
@@ -14,3 +14,8 @@ def verify_password(username, password):
     if user.user:
         return user.check_password(password)
     return False
+
+
+@bp.route('/register', method=['POST'])
+def register():  # TODO: 注册模块
+    pass
