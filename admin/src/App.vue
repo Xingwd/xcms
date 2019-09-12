a5838da1591f858b3b7fa6858c41982679f5ba55
Xingwd <614968414@qq.com>
2019-09-12 17:41:01 +0800

    开发布局-侧边栏

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-aside>
+        <NavMenu />
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </div>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+<script>
+import NavMenu from '@/components/NavMenu'
+
+export default {
+  components: {
+    NavMenu
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
