c1839d7164ccdebfeb61ae57a29f9e4febf3bb80
Xingwd <614968414@qq.com>
2019-09-25 17:03:21 +0800

    更新侧边栏导航的icon

--- admin/src/router.js
+++ admin/src/router.js
@@ -9,31 +9,31 @@ export default new Router({
       path: '/',
       name: 'home',
       component: () => import(/* webpackChunkName: "home" */ './views/Home.vue'),
-      meta: { icon: 'el-icon-location', title: 'Home' }
+      meta: { icon: 'el-icon-s-home', title: 'Home' }
     },
     {
       path: '/blog',
       name: 'blog',
       component: () => import(/* webpackChunkName: "blog" */ './views/blog/Blog.vue'),
-      meta: { icon: 'el-icon-location', title: '博客' },
+      meta: { icon: 'el-icon-notebook-1', title: '博客' },
       children: [
         {
           path: 'articleList',
           name: 'articleList',
           component: () => import(/* webpackChunkName: "blog" */ './views/blog/BlogList.vue'),
-          meta: { icon: 'el-icon-location', title: '博客列表' }
+          meta: { title: '博客列表' }
         },
         {
           path: 'createArticle',
           name: 'createArticle',
           component: () => import(/* webpackChunkName: "blog" */ './views/blog/CreateBlog.vue'),
-          meta: { icon: 'el-icon-location', title: '创建博客' }
+          meta: { title: '创建博客' }
         },
         {
           path: 'editArticle',
           name: 'editArticle',
           component: () => import(/* webpackChunkName: "blog" */ './views/blog/EditBlog.vue'),
-          meta: { icon: 'el-icon-location', title: '编辑博客' }
+          meta: { title: '编辑博客' }
         }
       ]
     },
@@ -41,7 +41,7 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-      meta: { icon: 'el-icon-location', title: 'About' }
+      meta: { icon: 'el-icon-goblet-full', title: 'About' }
     }
   ]
 })
