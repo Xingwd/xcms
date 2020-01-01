110b0645265b0c98459d60ba52a7d2d0eb816a03
xingweidong <xingweidong@redoop.com>
2020-01-01 16:59:32 +0800

    完成登录视图样式

--- web/src/views/auth/Login.vue
+++ web/src/views/auth/Login.vue
@@ -1,16 +1,26 @@
 <template>
-  <!-- TODO: 完善登陆视图 -->
-  <div class="x-bg">
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" size="small" class="x-login">
-      <el-form-item label="Username" prop="username">
-        <el-input v-model="ruleForm.username"></el-input>
+  <div class="x-login">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" autocomplete="on" label-position="left" size="medium" class="x-login-form">
+      <div class="title-container">
+        <h3 class="title">系统登录</h3>
+      </div>
+      <el-form-item prop="username">
+        <span class="el-icon-user x-login-icon" />
+        <el-input
+          v-model="ruleForm.username"
+          placeholder="Username"
+        />
       </el-form-item>
-      <el-form-item label="Password" prop="password">
-        <el-input v-model="ruleForm.password" show-password></el-input>
+      <el-form-item prop="password">
+        <span class="el-icon-key x-login-icon" />
+        <el-input
+          v-model="ruleForm.password"
+          placeholder="Password"
+          show-password
+        />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">Login</el-button>
-        <el-button @click="resetForm('ruleForm')">Reset</el-button>
+        <el-button type="primary" style="width:100%;" @click="submitForm('ruleForm')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -46,27 +56,89 @@ export default {
           return false
         }
       })
-    },
-    resetForm (formName) {
-      this.$refs[formName].resetFields()
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-@import '@/styles/variables.scss';
+<style lang="scss">
+$bg:#283443;
+$light_gray:#fff;
+$cursor: #fff;
 
-.x-bg {
-  height: 100%;
-  background-color: $menuBg;
-  .x-login {
-    width: 300px;
-    margin: 0 auto; /*水平居中*/
-    position: relative; /*脱离文档流*/
-    top: 50%; /*偏移*/
-    margin-top: -180px;
-    // background-color: $menuBg;
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .x-login .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.x-login {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+    [class^=el-icon-] {
+      font-weight: 1000;
+    }
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+
+.x-login {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+  .x-login-form {
+    position: relative;
+    width: 480px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  .x-login-icon {
+    padding: 6px 5px 6px 1px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
   }
 }
 </style>
--- web/src/views/auth/Register.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <!-- TODO: 开发注册视图 -->
-  <div class="register">
-    <h1>Register</h1>
-  </div>
-</template>
