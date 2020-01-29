e5e3766ad1937a59e33712beab4b7f85d51dd3aa
xingweidong <xingweidong@redoop.com>
2020-01-29 16:02:48 +0800

    Chore: 优化主页布局

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Power by Vue and Flask</h2>
   </div>
 </template>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -5,182 +5,147 @@
     </v-toolbar>
     <v-content>
       <section>
-        <v-parallax src="@/assets/hero.jpeg" height="600">
-          <v-layout
-            column
-            align-center
-            justify-center
-            class="white--text"
-          >
-            <img src="@/assets/vuetify.png" alt="Vuetify.js" height="200">
-            <h1 class="white--text mb-2 display-1 text-center">Parallax Template</h1>
-            <div class="subheading mb-4 text-center">Powered by Vuetify</div>
-            <v-btn
-              class="mt-12"
-              color="blue lighten-2"
-              dark
-              large
-              href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
-          </v-layout>
+        <v-parallax src="@/assets/home1.jpeg" height="600">
+          <v-row align="center">
+            <v-col align="center" >
+              <!-- <img src="@/assets/vuetify.png" alt="home-top" height="220"> -->
+              <v-icon x-large class="blue--text text--lighten-2 display-4 mb-7">mdi-cannabis</v-icon>
+              <div class="white--text mb-2 display-1">XCMS - 老邢的内容管理系统</div>
+              <div class="subheading mb-4">Xing Weidong's content management system</div>
+              <v-btn
+                class="mt-12"
+                color="blue lighten-2"
+                dark
+                large
+                href="https://github.com/Xingwd/xcms"
+              >
+                github
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-parallax>
       </section>
 
       <section>
-        <v-layout
-          column
-          wrap
-          class="my-12"
-          align-center
-        >
-          <v-flex xs12 sm4 class="my-4">
-            <div class="text-center">
-              <h2 class="headline">The best way to start developing</h2>
-              <span class="subheading">
-                Cras facilisis mi vitae nunc
-              </span>
-            </div>
-          </v-flex>
-          <v-flex xs12>
-            <v-container grid-list-xl>
-              <v-layout row wrap align-center>
-                <v-flex xs12 md4>
+        <v-row class="my-12" align="center">
+          <v-col xs12>
+            <v-container>
+              <v-row align="center">
+                <v-col align="center" xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2">mdi-palette</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-feather</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">Material Design</div>
+                      <div class="headline text-center">博客</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      学而不思则惘，思而不学则怠
                     </v-card-text>
                   </v-card>
-                </v-flex>
-                <v-flex xs12 md4>
+                </v-col>
+                <v-col align="center" xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2">mdi-flash</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-sprout</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline">Fast development</div>
+                      <div class="headline">学思</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      学而不思则惘，思而不学则怠
                     </v-card-text>
                   </v-card>
-                </v-flex>
-                <v-flex xs12 md4>
+                </v-col>
+                <v-col align="center" xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2">mdi-wrench</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-github-circle</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">Completely Open Sourced</div>
+                      <div class="headline text-center">项目</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      学而不思则惘，思而不学则怠
                     </v-card-text>
                   </v-card>
-                </v-flex>
-              </v-layout>
+                </v-col>
+              </v-row>
             </v-container>
-          </v-flex>
-        </v-layout>
+          </v-col>
+        </v-row>
       </section>
 
       <section>
-        <v-parallax src="@/assets/section.jpg" height="380">
-          <v-layout column align-center justify-center>
-            <div class="headline white--text mb-4 text-center">Web development has never been easier</div>
-            <em>Kick-start your application today</em>
-            <v-btn
-              class="mt-12"
-              color="blue lighten-2"
-              dark
-              large
-              href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
-          </v-layout>
+        <v-parallax src="@/assets/home2.jpg" height="380">
+          <v-row align="center">
+            <v-col align="center">
+              <div class="white--text mb-2 display-1">自强不息，厚德载物</div>
+              <div class="subheading">Self-discipline and Social Commitment</div>
+              <v-btn
+                class="mt-12"
+                color="blue lighten-2"
+                dark
+                large
+                href="https://github.com/Xingwd/xcms"
+              >
+                github
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-parallax>
       </section>
 
       <section>
-        <v-container grid-list-xl>
-          <v-layout row wrap justify-center class="my-12">
-            <v-flex xs12 sm4>
+        <v-container>
+          <v-row justify="center" class="mt-7 mb-12">
+            <v-col xs12 sm4 class="px-12">
               <v-card flat class="transparent">
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Company info</div>
+                  <div class="headline">声明</div>
                 </v-card-title>
                 <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                  <p>本站为个人内容管理系统，仅用于学习和参考，部分资源来源于网络，如有侵权，请联系我及时删除。</p>
+                  <p>站内内容，仅代表发布时的观点，不代表未来观点，如发现问题，请及时联系我。</p>
+                  <p>内容除非特别说明，否则均为原创，转载请注明出处。</p>
                 </v-card-text>
               </v-card>
-            </v-flex>
-            <v-flex xs12 sm4 offset-sm1>
+            </v-col>
+            <v-col xs12 sm4 class="px-12">
               <v-card flat class="transparent">
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
+                  <div class="headline">联系我</div>
                 </v-card-title>
                 <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  可通过以下方式联系本人。非必要情况，请勿打扰，谢谢。
                 </v-card-text>
                 <v-list class="transparent">
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>777-867-5309</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>Chicago, US</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
                   <v-list-item>
                     <v-list-item-action>
                       <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
-                      <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
+                      <v-list-item-title>1274296417@qq.com</v-list-item-title>
                     </v-list-item-content>
                   </v-list-item>
                 </v-list>
               </v-card>
-            </v-flex>
-          </v-layout>
+            </v-col>
+          </v-row>
         </v-container>
       </section>
 
-      <v-footer color="blue darken-2">
-        <v-layout row wrap align-center>
-          <v-flex xs12>
-            <div class="white--text ml-4">
-              Made with
-              <v-icon class="red--text">mdi-heart</v-icon>
-              by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-              and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
-            </div>
-          </v-flex>
-        </v-layout>
+      <v-footer
+        padless
+        absolute
+        class="font-weight-medium"
+      >
+        <v-col
+          class="text-center"
+          cols="12"
+        >
+          &copy; {{ new Date().getFullYear() }} — <strong>邢为栋</strong>
+        </v-col>
       </v-footer>
     </v-content>
   </v-app>
@@ -194,7 +159,7 @@ export default {
   },
   data () {
     return {
-      title: 'Your Logo'
+      title: 'XCMS'
     }
   }
 }
