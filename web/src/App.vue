0f2643a6f1a4b56a1b4e154d05903033892ae677
xingweidong <xingweidong@redoop.com>
2020-01-30 16:11:47 +0800

    Chore: 优化主页布局

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer
+      app
+      padless
+      absolute
+      color="#f5f5f5"
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        &copy; {{ new Date().getFullYear() }} — <strong>邢为栋</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,181 +1,166 @@
 <template>
-  <v-app light>
+  <div>
     <v-toolbar color="white">
       <v-toolbar-title v-text="title"></v-toolbar-title>
     </v-toolbar>
-    <v-content>
-      <section>
-        <v-parallax src="@/assets/home1.jpeg" height="600">
-          <v-row align="center">
-            <v-col align="center" >
-              <!-- <img src="@/assets/vuetify.png" alt="home-top" height="220"> -->
-              <v-icon x-large class="blue--text text--lighten-2 display-4 mb-7">mdi-cannabis</v-icon>
-              <div class="white--text mb-2 display-1">XCMS - 老邢的内容管理系统</div>
-              <div class="subheading mb-4">Xing Weidong's content management system</div>
-              <v-btn
-                class="mt-12"
-                color="blue lighten-2"
-                dark
-                large
-                href="https://github.com/Xingwd/xcms"
-                target="_blank"
-              >
-                github
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-parallax>
-      </section>
+    <section>
+      <v-parallax src="@/assets/home1.jpeg" height="600">
+        <v-row align="center">
+          <v-col align="center" >
+            <!-- <img src="@/assets/vuetify.png" alt="home-top" height="220"> -->
+            <v-icon x-large class="blue--text text--lighten-2 display-4 mb-7">mdi-cannabis</v-icon>
+            <div class="white--text mb-2 display-1">XCMS - 邢的内容管理系统</div>
+            <div class="subheading mb-4">Xing Weidong's content management system</div>
+            <v-btn
+              class="mt-12"
+              color="blue lighten-2"
+              dark
+              large
+              href="https://github.com/Xingwd/xcms"
+              target="_blank"
+            >
+              github
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-parallax>
+    </section>
 
-      <section>
-        <v-container>
-          <v-row class="my-12" justify="center">
-            <v-col xs12 sm4>
-              <v-card
-                flat
-                class="transparent"
-                to="about"
-              >
-                <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2">mdi-feather</v-icon>
-                </v-card-text>
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline text-center">博客</div>
-                </v-card-title>
-                <v-card-text>
-                  技术博客，记录学习和工作中遇到的一些问题，作为分享和备忘。
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <!-- <v-col align="center" xs12 sm4>
-              <v-card
-                flat
-                class="transparent"
-                to="about"
-              >
-                <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2">mdi-sprout</v-icon>
-                </v-card-text>
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline">学思</div>
-                </v-card-title>
-                <v-card-text>
-                  学而不思则惘，思而不学则怠
-                </v-card-text>
-              </v-card>
-            </v-col> -->
-            <v-col align="center" xs12 sm4>
-              <v-card
-                flat
-                class="transparent"
-                to="about"
-              >
-                <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2">mdi-github-circle</v-icon>
-                </v-card-text>
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline text-center">项目</div>
-                </v-card-title>
-                <v-card-text>
-                  一些独立开发或者二次开发的项目。
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col align="center" xs12 sm4>
-              <v-card
-                flat
-                class="transparent"
-                to="about"
-              >
-                <v-card-text class="text-center">
-                  <v-icon x-large class="blue--text text--lighten-2">mdi-seed</v-icon>
-                </v-card-text>
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline text-center">关于</div>
-                </v-card-title>
-                <v-card-text>
-                  一些说明，包括本站的设计，使用到的框架等。
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </section>
+    <section>
+      <v-container>
+        <v-row class="my-12" justify="center">
+          <v-col xs12 sm4>
+            <v-card
+              flat
+              class="transparent"
+              to="blog"
+            >
+              <v-card-text class="text-center">
+                <v-icon x-large class="blue--text text--lighten-2">mdi-feather</v-icon>
+              </v-card-text>
+              <v-card-title primary-title class="justify-center">
+                <div class="headline text-center">博客</div>
+              </v-card-title>
+              <v-card-text>
+                技术博客，记录学习和工作中遇到的一些问题，作为分享和备忘。
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <!-- <v-col align="center" xs12 sm4>
+            <v-card
+              flat
+              class="transparent"
+              to="xuesi"
+            >
+              <v-card-text class="text-center">
+                <v-icon x-large class="blue--text text--lighten-2">mdi-sprout</v-icon>
+              </v-card-text>
+              <v-card-title primary-title class="justify-center">
+                <div class="headline">学思</div>
+              </v-card-title>
+              <v-card-text>
+                学而不思则惘，思而不学则怠
+              </v-card-text>
+            </v-card>
+          </v-col> -->
+          <v-col align="center" xs12 sm4>
+            <v-card
+              flat
+              class="transparent"
+              to="project"
+            >
+              <v-card-text class="text-center">
+                <v-icon x-large class="blue--text text--lighten-2">mdi-github-circle</v-icon>
+              </v-card-text>
+              <v-card-title primary-title class="justify-center">
+                <div class="headline text-center">项目</div>
+              </v-card-title>
+              <v-card-text>
+                一些独立开发或者二次开发的项目。
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col align="center" xs12 sm4>
+            <v-card
+              flat
+              class="transparent"
+              to="about"
+            >
+              <v-card-text class="text-center">
+                <v-icon x-large class="blue--text text--lighten-2">mdi-seed</v-icon>
+              </v-card-text>
+              <v-card-title primary-title class="justify-center">
+                <div class="headline text-center">关于</div>
+              </v-card-title>
+              <v-card-text>
+                一些简单说明，包括本站的设计，使用到的框架等。
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
 
-      <section>
-        <v-parallax src="@/assets/home2.jpg" height="380">
-          <v-row align="center">
-            <v-col align="center">
-              <div class="white--text mb-2 display-1">自强不息，厚德载物</div>
-              <div class="subheading">Self-discipline and Social Commitment</div>
-              <v-btn
-                class="mt-12"
-                color="blue lighten-2"
-                dark
-                large
-                href="https://github.com/Xingwd/xcms"
-                target="_blank"
-              >
-                github
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-parallax>
-      </section>
+    <section>
+      <v-parallax src="@/assets/home2.jpg" height="380">
+        <v-row align="center">
+          <v-col align="center">
+            <div class="white--text mb-2 display-1">自强不息，厚德载物</div>
+            <div class="subheading">Self-discipline and Social Commitment</div>
+            <v-btn
+              class="mt-12"
+              color="blue lighten-2"
+              dark
+              large
+              href="https://github.com/Xingwd/xcms"
+              target="_blank"
+            >
+              github
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-parallax>
+    </section>
 
-      <section>
-        <v-container>
-          <v-row justify="center" class="mt-7 mb-12">
-            <v-col xs12 sm4 class="px-12">
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline">声明</div>
-                </v-card-title>
-                <v-card-text>
-                  <p>本站为个人内容管理系统，仅用于学习和参考，部分资源来源于网络，如有侵权，请联系我及时删除。</p>
-                  <p>站内内容，仅代表发布时的观点，不代表未来观点，如发现问题，请及时联系我。</p>
-                  <p>内容除非特别说明，否则均为原创，转载请注明出处。</p>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col xs12 sm4 class="px-12">
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="justify-center">
-                  <div class="headline">联系我</div>
-                </v-card-title>
-                <v-card-text>
-                  可通过以下方式联系本人。非必要情况，请勿打扰，谢谢。
-                </v-card-text>
-                <v-list class="transparent">
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>1274296417@qq.com</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </section>
-
-      <v-footer
-        padless
-        absolute
-        class="font-weight-medium"
-      >
-        <v-col
-          class="text-center"
-          cols="12"
-        >
-          &copy; {{ new Date().getFullYear() }} — <strong>邢为栋</strong>
-        </v-col>
-      </v-footer>
-    </v-content>
-  </v-app>
+    <section>
+      <v-container>
+        <v-row justify="center" class="mt-7 mb-12">
+          <v-col xs12 sm4 class="px-12">
+            <v-card flat class="transparent">
+              <v-card-title primary-title class="justify-center">
+                <div class="headline">声明</div>
+              </v-card-title>
+              <v-card-text>
+                <p>本站为个人内容管理系统，仅用于学习和参考，部分资源来源于网络，如有侵权，请联系我及时删除。</p>
+                <p>站内内容，仅代表发布时的观点，不代表未来观点，如发现问题，请及时联系我。</p>
+                <p>内容除非特别说明，否则均为原创，转载请注明出处。</p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col xs12 sm4 class="px-12">
+            <v-card flat class="transparent">
+              <v-card-title primary-title class="justify-center">
+                <div class="headline">联系我</div>
+              </v-card-title>
+              <v-card-text>
+                可通过以下方式联系本人。非必要情况，请勿打扰，谢谢。
+              </v-card-text>
+              <v-list class="transparent">
+                <v-list-item>
+                  <v-list-item-action>
+                    <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-content>
+                    <v-list-item-title>1274296417@qq.com</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+  </div>
 </template>
 
 <script>
