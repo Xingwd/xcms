32f07a0f1f2ccf12a85620ff443743a1351813ed
xingweidong <xingweidong@redoop.com>
2020-02-03 09:31:22 +0800

    Chore: 优化历程页面信息

--- web/src/views/History.vue
+++ web/src/views/History.vue
@@ -30,27 +30,28 @@
 
 <script>
 export default {
+  // TODO: 使用后台管理数据
   data: () => ({
     histories: [
       {
-        time: '2020',
-        title: 'v1.0',
-        text: '前端分为Admin和Web两部分：Admin部分使用Element框架开发；Web部分使用Vuetify框架开发。'
+        time: '2020.02',
+        title: 'v1.0.0',
+        text: '重新规划项目结构，使用MVVM架构设计思想，后端使用Flask，前端使用Vue。前端分为Admin和Web两部分：Admin部分使用Element框架开发；Web部分使用Vuetify框架开发。'
       },
       {
-        time: '2019',
-        title: 'v1.0_dev',
-        text: '重新规划项目结构，使用MVVM架构设计思想，后端使用Flask，前端使用Vue。'
+        time: '2019.04',
+        title: 'v0.3.1',
+        text: '优化结构，优化部分功能的展示效果。完善博客功能。'
       },
       {
-        time: '2018',
-        title: 'v0.3',
-        text: '在v0.1的基础上，使用Bootstrap重新开发前端，并增加部分功能。'
+        time: '2018.11',
+        title: 'v0.2.7',
+        text: '增加博客管理及展示功能，增加网站背景动画。'
       },
       {
-        time: '2017',
-        title: 'v0.1',
-        text: '初始化xcms工程，使用MVC架构设计思想，通过Flask框架构建个人博客。'
+        time: '2018.10',
+        title: 'v0.1.0',
+        text: '初始化xcms工程，使用MVC架构设计思想，通过Flask和Bootstrap构建个人网站。增加后台管理基本功能。'
       }
     ]
   })
